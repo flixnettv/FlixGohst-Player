8c78aca1a0541053e88e555ead523b8cae27a519
FlixNet TV <flixnettv@gmail.com>
2026-07-07 22:46:29 +0300

    use flat functions/ directory (status.js, activate.js)

---
+++ functions/activate.js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  const targetUrl = 'http://65.75.200.19:3002/api/device/activate' + url.search;
+  try {
+    const response = await fetch(targetUrl, { method: request.method, headers: request.headers, body: request.body });
+    const respHeaders = new Headers(response.headers);
+    respHeaders.set('Access-Control-Allow-Origin', '*');
+    return new Response(response.body, { status: response.status, headers: respHeaders });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: 'Backend unreachable' }), {
+      status: 502, headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
