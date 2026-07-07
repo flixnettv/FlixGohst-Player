b9d4a1d2c2248e72502a7b2707e3a938baaecd5d
FlixNet TV <flixnettv@gmail.com>
2026-07-07 22:00:42 +0300

    use functions/api/[...path].js catch-all for API proxy

--- functions/api/hello.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ status: 'ok', message: 'Pages Function works!' }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
