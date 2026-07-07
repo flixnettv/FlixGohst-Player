5d687ec402815eab878822affcea6e29b3e1e4d5
FlixNet TV <flixnettv@gmail.com>
2026-07-07 23:03:00 +0300

    revert to simple device functions (no fetch yet)

--- functions/api/device/activate.js
+++ functions/api/device/activate.js
@@ -1,14 +1,5 @@
 export async function onRequest(context) {
-  const { request } = context;
-  const url = new URL(request.url);
-  const targetUrl = 'http://65.75.200.19:3002' + url.pathname + url.search;
-  try {
-    const response = await fetch(targetUrl, { method: request.method, body: request.body, headers: request.headers });
-    return new Response(response.body, { status: response.status, headers: response.headers });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: 'Backend unreachable' }), {
-      status: 502,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+  return new Response(JSON.stringify({ status: "ok", msg: "device/activate works" }), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
