ba5332896544bbc58e329d1d2630d6813b823f25
FlixNet TV <flixnettv@gmail.com>
2026-07-07 23:56:43 +0300

    fix: forward request headers in Pages Functions (fixes POST body parsing); improve error display

--- functions/api/playlist/delete.js
+++ functions/api/playlist/delete.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const url = new URL(request.url);
   const targetUrl = 'https://api.fgtv.qzz.io' + url.pathname + url.search;
   try {
-    const response = await fetch(targetUrl, { method: request.method, body: request.method === 'GET' ? undefined : request.body });
+    const response = await fetch(targetUrl, { method: request.method, headers: request.headers, body: request.method === 'GET' ? undefined : request.body });
     const respHeaders = new Headers(response.headers);
     respHeaders.set('Access-Control-Allow-Origin', '*');
     respHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
