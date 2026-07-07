5ccc94f0adfd2a27a73b57a553f3e26961041b5a
FlixNet TV <flixnettv@gmail.com>
2026-07-07 22:55:15 +0300

    test: simple nested /api/test function (npm run build)

--- functions/test.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ status: "ok", msg: "root function works" }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
